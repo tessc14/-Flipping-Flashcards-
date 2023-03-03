bd036cce5038fb2a91d9ad57d8f5bcdbcddee120
kay-dev-uk <kay-dev-uk@gmail.com>
2023-03-03 22:02:59 +0000

    set up basic Routes, create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 // import './App.css'
 import React from 'react'
+import * as Pages from './pages'
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <>
-
+      <Routes>
+          <Route index element={<Pages.Home />} />
+          <Route path='/flashcards' element={<Pages.Flashcards />} />
+          <Route path='/add' element={<Pages.AddFlashcard />} />
+          <Route path='/login' element={<Pages.Login />} />
+          <Route path='/categories' element={<Pages.SetCategory />} />
+          <Route path='*' element={<Pages.NotFound />} />
+      </Routes>
 
     </>
   )
---
+++ client/src/pages/index.js
@@ -0,0 +1,6 @@
+export { default as AddFlashcard } from './AddFlashcard'
+export { default as Flashcards } from './Flashcards'
+export { default as Home } from './Home'
+export { default as Login } from './Login'
+export { default as SetCategory } from './SetCategory'
+export { default as NotFound } from './NotFound'
