6bf6bffc7d13626f727ac32243352d77141e31b5
Baskin-Ridley <gbaskinridley@gmail.com>
2023-03-06 14:36:04 +0000

    fix setup export/import

--- api/database/setup.js
+++ api/database/setup.js
@@ -1,7 +1,7 @@
 import fs from "fs";
 import dotenv from "dotenv";
 
-import db from "./connect";
+import db from "./connect.js";
 dotenv.config();
 
 const sql = fs.readFileSync("./database/setup.sql").toString();
@@ -9,3 +9,5 @@ const sql = fs.readFileSync("./database/setup.sql").toString();
 db.query(sql)
   .then((data) => console.log("Set-up complete."))
   .catch((error) => console.log(error));
+
+export default db;
