3879c1776cefb1f07bf1adb60148873b31202076
Baskin-Ridley <gbaskinridley@gmail.com>
2023-03-06 16:58:20 +0000

    add more tests

--- client/src/pages/Login/login.test.jsx
+++ client/src/pages/Login/login.test.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import Login from "./index";
@@ -6,5 +6,24 @@ import Login from "./index";
 describe("Login", () => {
   it("exists", () => {
     expect(Login).toBeDefined();
+  });
+  it("renders", () => {
+    render(<Login />);
+  });
+  it("renders a form", () => {
+    render(<Login />);
+    expect(screen.getByRole("form")).toBeDefined();
+  });
+  it("renders a username input", () => {
+    render(<Login />);
+    expect(screen.getByLabelText("Username")).toBeDefined();
+  });
+  it("renders a password input", () => {
+    render(<Login />);
+    expect(screen.getByLabelText("Password")).toBeDefined();
+  });
+  it("renders a submit button", () => {
+    render(<Login />);
+    expect(screen.getByRole("button")).toBeDefined();
   });
 });
