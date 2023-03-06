e6ff2266b61cf54aab1e9a58cbf308ae9659a306
kay-dev-uk <kay-dev-uk@gmail.com>
2023-03-06 15:35:59 +0000

    fix api

--- api/api.js
+++ api/api.js
@@ -1,8 +1,5 @@
 import express from "express";
 import cors from "cors";
-
-import path from "path";
-import { fileURLToPath } from "url";
 import flashcardsRouter from "./routers/flashcards.js";
 
 const api = express();
@@ -10,13 +7,13 @@ const api = express();
 api.use(cors());
 api.use(express.json());
 
-/*
+
 api.get("/", (req, res) => {
   res.json({
     description: "Flipping Flashcards API",
   });
 });
-*/
+
 api.use("/flashcards", flashcardsRouter);
 
 export default api;
