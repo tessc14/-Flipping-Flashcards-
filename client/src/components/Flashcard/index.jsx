21e5c4582c376d0e85485c6754d20e8b958d6687
tessc14 <tessc14@gmail.com>
2023-03-06 16:59:13 +0000

    display flashcards

--- client/src/components/Flashcard/index.jsx
+++ client/src/components/Flashcard/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './style.css'
 
-function Flashcard(key, flash_id, user_id, question, answer, category ) {
+function Flashcard({ question, answer} ) {
   return (
     <div className="flashcard">
       <h1>{question}</h1>
--- client/src/components/FlashcardGallery/index.jsx
+++ client/src/components/FlashcardGallery/index.jsx
@@ -4,27 +4,26 @@ import './style.css'
 
 function FlashcardGallery() {
 
-  const [flashcards, setFlashcards] = useState()
+  const [flashcards, setFlashcards] = useState([])
+
+  async function loadFlashcards() {
+      const response = await fetch("http://localhost:3000/flashcards");
+      const data = await response.json();
+      setFlashcards(data);
+      console.log(flashcards)
+  };
 
   useEffect(() => {
-    async function loadFlashcards() {
-        const response = await fetch("http://localhost:3000/flashcards");
-        const data = await response.json();
-        setFlashcards(data);
-        console.log(flashcards)
-    };
-    
     loadFlashcards();
   }, [])
 
-  // function displayFlashcards() {
-  //     return flashcards.map(f => <Flashcard key={f.id} flash_id={f.flashcard_id} user_id={f.user_id} question={f.question} answer={f.answer} category={f.category} />)
-  // }
+  function displayFlashcards() {
+      return flashcards.map((f) => <Flashcard key={f.id} question={f.question} answer={f.answer} />)
+  }
 
   return (
     <div className="flashcard-container">
-      <Flashcard />
-        {/* {displayFlashcards()} */}
+        {displayFlashcards()}
     </div>
   )
 }
