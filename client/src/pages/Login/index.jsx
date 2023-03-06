002b80fd8ac6ad41e6106805b0d73fb9dbb655e0
Baskin-Ridley <gbaskinridley@gmail.com>
2023-03-06 15:20:18 +0000

    create login page

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,18 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
-  const [useName, setUserName] = useState("");
+  const [userName, setUserName] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(true);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("email", email);
   };
+  console.log("object");
 
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <>
+      <div>
+        {isLogin ? (
+          <>
+            <h1>Register</h1>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="userName">Username</label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <input
+                type="password"
+                name="confirmPassword"
+                id="confirmPassword"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
+              <button type="submit">Register</button>
+              <button onClick={() => setIsLogin(!isLogin)}>Go to Login</button>
+            </form>
+          </>
+        ) : (
+          <>
+            <h1>Login</h1>
+            <form onSubmit={handleSubmit}>
+              <label htmlFor="userName">UserName</label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
+              />
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit">Login</button>
+              <button onClick={() => setIsLogin(!isLogin)}>
+                Go to Register
+              </button>
+            </form>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 
