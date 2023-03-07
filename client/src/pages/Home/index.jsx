469a4a2df99266a62fcf243d65b184061fc3173e
Baskin-Ridley <gbaskinridley@gmail.com>
2023-03-07 15:39:23 +0000

    navigate to login on page load

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
+import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../../contexts";
 const Home = () => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+
+  console.log(user);
+
+  useEffect(() => {
+    if (user === null) {
+      navigate("/login");
+    }
+  }, []);
+
   return (
     <>
       <h2>Welcome</h2>
       <h3>Learn faster with our interactive flashcards!</h3>
-      <h4><Link to='/flashcards' className='home-links'>Start Flipping Flashcards</Link></h4>
-      <h4><Link to='/add' className='home-links'>Add a new flashcard</Link></h4>
-      <h4><Link to='/categories' className='home-links'>Categories</Link></h4>
+      <h4>
+        <Link to="/flashcards" className="home-links">
+          Start Flipping Flashcards
+        </Link>
+      </h4>
+      <h4>
+        <Link to="/add" className="home-links">
+          Add a new flashcard
+        </Link>
+      </h4>
+      <h4>
+        <Link to="/categories" className="home-links">
+          Categories
+        </Link>
+      </h4>
       {/* <Link to='/login'>Login</Link> */}
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
