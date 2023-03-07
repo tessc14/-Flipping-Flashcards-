ebbcaf9faa1ebf8f7376221e6d3da88689940d85
tessc14 <tessc14@gmail.com>
2023-03-07 10:46:01 +0000

    give form functions

--- client/src/components/NewFlashcardForm/index.jsx
+++ client/src/components/NewFlashcardForm/index.jsx
@@ -1,19 +1,37 @@
 import React from 'react'
+import './style.css'
 
-function NewFlashcardForm() {
+function NewFlashcardForm({question, answer, setQuestion, setAnswer}) {
+
+
+    function handleQuestion(e) {
+        setQuestion(e.target.value)
+    }
+
+    function handleAnswer(e) {
+        setAnswer(e.target.value)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        console.log(question, answer)
+        setAnswer('')
+        setQuestion('')
+    }
+
   return (
     <div>
         <form action="submit">
-            <label for="category">Choose a category:</label>
+            <label htmlFor="category">Choose a category:</label>
             <select id="category" name="category">
                 <option value="History">History</option>
                 <option value="Geography">Geography</option>
             </select>
             <label htmlFor="question">Question</label>
-            <input type="text" value="Question" id="question"/>
+            <input type="text" value={question} id="question" onChange={handleQuestion}/>
             <label htmlFor="answer">Answer</label>
-            <input type="text" value="Answer" id="answer" />
-            <input type="submit" />
+            <input type="text" value={answer} id="answer" onChange={handleAnswer}/>
+            <button onClick={handleSubmit}>Add flashcard</button>
         </form>
     </div>
   )
--- client/src/pages/AddFlashcard/index.jsx
+++ client/src/pages/AddFlashcard/index.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NewFlashcardForm } from '../../components'
 
 const AddFlashcard = () => {
+  const [questionInput, setQuestionInput] = useState('')
+  const [answerInput, setAnswerInput] = useState('')
+
+
   return (
     <div>
       <h1>Add a new flashcard:</h1>
       <div>
-        <NewFlashcardForm />
+        <NewFlashcardForm question={questionInput} setQuestion={setQuestionInput} answer={answerInput} setAnswer={setAnswerInput}/>
       </div>
     </div>
   )
