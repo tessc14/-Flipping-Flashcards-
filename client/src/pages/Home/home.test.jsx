64910f7542bb9adb0c9856175f1483dae3e3bacd
Baskin-Ridley <gbaskinridley@gmail.com>
2023-03-08 15:34:05 +0000

    add tests

---
+++ client/src/pages/Home/home.test.jsx
@@ -0,0 +1,8 @@
+import Home from "./index";
+import { describe, it, expect } from "vitest";
+
+describe("Home", () => {
+  it("exists", () => {
+    expect(Home).toBeDefined();
+  });
+});
---
+++ client/src/pages/NotFound/notFound.test.jsx
@@ -0,0 +1,8 @@
+import notFound from "./index";
+import { describe, it, expect } from "vitest";
+
+describe("notFound", () => {
+  it("exists", () => {
+    expect(notFound).toBeDefined();
+  });
+});
